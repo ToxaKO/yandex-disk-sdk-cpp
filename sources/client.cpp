18f7ff8edf6ac8ddd025c1c7238195f109256b76
ToxaKO <komarov.05.16@mail.ru>
2017-03-06 21:54:39 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -20,53 +20,53 @@ namespace yadisk
 
     Client::Client(string token_) : token{token_} {}
     
-    auto upload(url::path to, fs::path from, bool overwrite, std::list<string> fields) -> json {
-    CURL *curl;
-  CURLcode res;
-  struct stat file_info;
-  double speed_upload, total_time;
-  FILE *fd;
-  url::params_t url_params;
-		struct curl_slist *header_list = nullptr;
-		std::string auth_header;
- 
-		url_params["path"] = quote(to.string(), curl);
-		url_params["overwrite"] = overwrite;
-		url_params["fields"] = boost::algorithm::join(fields, ",");
-		std::string url = api_url + "/copy?" + url_params.string();
-	    
-	    struct curl_slist *head_list = nullptr;
-  auth_head = "Authorization: OAuth " + token;
-		head_list = curl_slist_append(head_list, auth_head.c_str());
+    auto Client::upload(url::path to, fs::path from, bool overwrite, std::list<string> fields) -> json {
+   	 CURL *curl;
+  	
+  	struct stat file_info;
   
-  fd = fopen("debugit", "rb");  //открытие файла для загрузки
- /* if(!fd)
-    return 1; */
+  	FILE *fd;
+ 	url::params_t url_params;
+	struct curl_slist *header_list = nullptr;
+	std::string auth_header;
+	    
+ 	fs::path p(fs::current_path());
+	url_params["path"] = quote(to.string(), curl);
+	url_params["overwrite"] = overwrite;
+	url_params["fields"] = boost::algorithm::join(fields, ",");
+	std::string url = api_url + "/upload?" + url_params.string();
+	    
+	struct curl_slist *head_list = nullptr;
+  	auth_head = "Authorization: OAuth " + token;
+	head_list = curl_slist_append(head_list, auth_head.c_str());
+  
+  fd = fopen("file.txt", "w");  //открытие файла для загрузки
+ 
    // получаем размер файла
-  if(fstat(fileno(fd), &file_info) != 0)
+  fstat(fileno(fd), &file_info;
   //  return 1; /* can't continue */ 
  
   curl = curl_easy_init();
 //  if(curl) {
      //выбор места для загрузки
-    curl_easy_setopt(curl, CURLOPT_URL,url.c_str());
-    curl_easy_setopt(curl, CURLOPT_READDATA, &response);              
-    curl_easy_setopt(curl, CURLOPT_READFUNCTION, write<stringstream>);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, head_list);
+    	curl_easy_setopt(curl, CURLOPT_URL,url.c_str());
+    	curl_easy_setopt(curl, CURLOPT_READDATA, &response);              
+    	curl_easy_setopt(curl, CURLOPT_READFUNCTION, write<stringstream>);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, head_list);
      //загрузка по URL
-    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
- 
+    	curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+ 	curl_easy_setopt(curl, CURLOPT_PUT, 1L);
      //чтения файла 
-    curl_easy_setopt(curl, CURLOPT_READDATA, fd);
+    	curl_easy_setopt(curl, CURLOPT_READDATA, fd);
  
     //устанавливаем размер файла для отправки
-    curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
+    	curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
                      (curl_off_t)file_info.st_size);
  
      //для облегчения трассировки 
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
  
     auto response_code = curl_easy_perform(curl);
 
@@ -75,7 +75,7 @@ namespace yadisk
 
 		if (response_code != CURLE_OK) return json();
 
-		auto answer = json::parse(response);
+		auto answer = json::parse(response_code);
 		return answer;
     
 }
