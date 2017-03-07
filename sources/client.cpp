9b65798364890f28b905bd67cbcdad8a8c641401
ToxaKO <komarov.05.16@mail.ru>
2017-03-07 10:53:39 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -20,110 +20,62 @@ namespace yadisk
 
     Client::Client(string token_) : token{token_} {}
     
-    auto upload(url::path to, fs::path from, bool overwrite, std::list<string> fields) -> json {
-    CURL *curl;
-  CURLcode res;
-  struct stat file_info;
-  double speed_upload, total_time;
-  FILE *fd;
-  url::params_t url_params;
-		struct curl_slist *header_list = nullptr;
-		std::string auth_header;
- 
-		url_params["path"] = quote(to.string(), curl);
-		url_params["overwrite"] = overwrite;
-		url_params["fields"] = boost::algorithm::join(fields, ",");
-		std::string url = api_url + "/copy?" + url_params.string();
-	    
-	    struct curl_slist *head_list = nullptr;
-  auth_head = "Authorization: OAuth " + token;
-		head_list = curl_slist_append(head_list, auth_head.c_str());
+    auto Client::upload(url::path to, fs::path from, bool overwrite, std::list<string> fields) -> json {
+   	 CURL *curl;
+  	
+  	struct stat file_info;
   
-  fd = fopen("debugit", "rb");  //открытие файла для загрузки
- /* if(!fd)
-    return 1; */
-   // получаем размер файла
-  if(fstat(fileno(fd), &file_info) != 0)
-  //  return 1; /* can't continue */ 
+  	FILE *fd;
+ 	url::params_t url_params;
+	struct curl_slist *header_list = nullptr;
+	std::string auth_header;
+	    
+ 	fs::path p(fs::current_path());
+	url_params["path"] = quote(to.string(), curl);
+	url_params["overwrite"] = overwrite;
+	url_params["fields"] = boost::algorithm::join(fields, ",");
+	std::string url = api_url + "/upload?" + url_params.string();
+	    
+	struct curl_slist *head_list = nullptr;
+  	auth_head = "Authorization: OAuth " + token;
+	head_list = curl_slist_append(head_list, auth_head.c_str());
+  
+  	fd = fopen("C:\\file.txt", "w");  //открытие файла для загрузки
+ 	if(!fd)
+    	return 1; //не может продолжить 
+   	// получаем размер файла
+  	if(fstat(fileno(fd), &file_info)!=0;
+    	return 1; /* can't continue */ 
  
-  curl = curl_easy_init();
-//  if(curl) {
-     //выбор места для загрузки
-    curl_easy_setopt(curl, CURLOPT_URL,url.c_str());
-    curl_easy_setopt(curl, CURLOPT_READDATA, &response);              
-    curl_easy_setopt(curl, CURLOPT_READFUNCTION, write<stringstream>);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, head_list);
-     //загрузка по URL
-    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
- 
-     //чтения файла 
-    curl_easy_setopt(curl, CURLOPT_READDATA, fd);
- 
-    //устанавливаем размер файла для отправки
-    curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
-                     (curl_off_t)file_info.st_size);
- 
-     //для облегчения трассировки 
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
- 
-    auto response_code = curl_easy_perform(curl);
-
-		curl_slist_free_all(head_list);
-		curl_easy_cleanup(curl);
-
-		if (response_code != CURLE_OK) return json();
-
-		auto answer = json::parse(response);
-		return answer;
-    
+  	curl = curl_easy_init();
+	//  if(curl) {
+     	//выбор места для загрузки
+    	curl_easy_setopt(curl, CURLOPT_URL,url.c_str());
+    	curl_easy_setopt(curl, CURLOPT_READDATA, &response);              
+    	curl_easy_setopt(curl, CURLOPT_READFUNCTION, write<stringstream>);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, head_list);
+    	auto res = curl_easy_perform(curl);
+     	curl_easy_cleanup(curl);
+     	if (res != CURLE_OK) return json();
+	auto ans = json::parse(res);
+	return ans;
+    	 //загрузка по URL
+    	curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+ 	curl_easy_setopt(curl, CURLOPT_PUT, 1L);
+     	//чтения файла 
+    	curl_easy_setopt(curl, CURLOPT_READDATA, fd);
+    	//устанавливаем размер файла для отправки
+    	curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,(curl_off_t)file_info.st_size);
+     	//для облегчения трассировки 
+    	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    	auto response_code = curl_easy_perform(curl);
+	curl_slist_free_all(head_list);
+	curl_easy_cleanup(curl);
+	if (response_code != CURLE_OK) return json();
+	auto answer = json::parse(response_code);
+	return answer;
+     	return 0;    
 }
-
-    auto Client::patch(url::path resource, json meta, std::list<string> fields) -> json {
-
-        // init http request
-        CURL * curl = curl_easy_init();
-
-        // fill http url
-        url::params_t url_params;
-        url_params["fields"] = boost::algorithm::join(fields, ",");
-        url_params["path"] = quote(resource.string(), curl);
-        std::string url = api_url + "?" + url_params.string();
-
-        // fill http header
-        curl_slist * http_header = nullptr;
-        std::string authorization_header = "Authorization: OAuth " + token;
-        http_header = curl_slist_append(http_header, "Content-Type: application/json");
-        http_header = curl_slist_append(http_header, authorization_header.c_str());
-
-        // fill http body
-        auto request_body = meta.dump();
-
-        // build http request
-        stringstream response_body;
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PATCH");
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_body);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write<stringstream>);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request_body.c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, request_body.size());
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, http_header);
-
-        // perform http request
-        auto response_code = curl_easy_perform(curl);
-
-        // clean resources
-        curl_slist_free_all(http_header);
-        curl_easy_cleanup(curl);
-
-        // check response code
-        if ( response_code != CURLE_OK ) return json();
-
-        // handle body of http response
-        auto info = json::parse(response_body);
-        return info;
-    }
 }
