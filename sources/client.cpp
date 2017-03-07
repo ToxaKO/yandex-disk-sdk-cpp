5c28b3e92151495e50eeaf2230a7a611d30b56b0
ToxaKO <komarov.05.16@mail.ru>
2017-03-07 10:23:10 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -40,43 +40,42 @@ namespace yadisk
   	auth_head = "Authorization: OAuth " + token;
 	head_list = curl_slist_append(head_list, auth_head.c_str());
   
-  fd = fopen("file.txt", "w");  //открытие файла для загрузки
+  	fd = fopen("C:\\file.txt", "w");  //открытие файла для загрузки
+ 	if(!fd)
+    	return 1; //не может продолжить 
+   	// получаем размер файла
+  	if(fstat(fileno(fd), &file_info)!=0;
+    	return 1; /* can't continue */ 
  
-   // получаем размер файла
-  fstat(fileno(fd), &file_info;
-  //  return 1; /* can't continue */ 
- 
-  curl = curl_easy_init();
-//  if(curl) {
-     //выбор места для загрузки
+  	curl = curl_easy_init();
+	//  if(curl) {
+     	//выбор места для загрузки
     	curl_easy_setopt(curl, CURLOPT_URL,url.c_str());
     	curl_easy_setopt(curl, CURLOPT_READDATA, &response);              
     	curl_easy_setopt(curl, CURLOPT_READFUNCTION, write<stringstream>);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, head_list);
-     //загрузка по URL
+    	auto res = curl_easy_perform(curl);
+     	curl_easy_cleanup(curl);
+     	if (res != CURLE_OK) return json();
+	auto ans = json::parse(res);
+	return ans;
+    	 //загрузка по URL
     	curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
  	curl_easy_setopt(curl, CURLOPT_PUT, 1L);
-     //чтения файла 
+     	//чтения файла 
     	curl_easy_setopt(curl, CURLOPT_READDATA, fd);
- 
-    //устанавливаем размер файла для отправки
-    	curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
-                     (curl_off_t)file_info.st_size);
- 
-     //для облегчения трассировки 
+    	//устанавливаем размер файла для отправки
+    	curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,(curl_off_t)file_info.st_size);
+     	//для облегчения трассировки 
     	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
- 
-    auto response_code = curl_easy_perform(curl);
-
-		curl_slist_free_all(head_list);
-		curl_easy_cleanup(curl);
-
-		if (response_code != CURLE_OK) return json();
-
-		auto answer = json::parse(response_code);
-		return answer;
-    
+    	auto response_code = curl_easy_perform(curl);
+	curl_slist_free_all(head_list);
+	curl_easy_cleanup(curl);
+	if (response_code != CURLE_OK) return json();
+	auto answer = json::parse(response_code);
+	return answer;
+     	return 0;    
 }
 }
